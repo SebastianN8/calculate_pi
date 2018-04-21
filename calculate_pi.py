c653bd8b9368d55e04c229a0ba058ff35808e725
SebastianN8 <sebastian.navas@ocsbstudent.ca>
2018-04-21 12:34:49 -0400

    Informative comments

--- calculate_pi.py
+++ calculate_pi.py
@@ -6,16 +6,18 @@
 #
 # This program calculates pi according to iterations
 #
+
+# This is where math.floor comes from
 import math
 
-# Function
+# Function that contains the loop in order to get the result of a gregory leibniz series
 def calculate_pi(iterations_passed_in):
-	# Variables
+	# Variables that will carry the count in the loop, the resul of the addition and the result for pi.
 	the_iterations = 0
 	addition = 0
 	pi = 0
 
-	# Loop
+	# If statement that takes care of filtering inputs such as 0 and decimal numbers.
 	if 	(iterations_passed_in == math.floor(iterations_passed_in)) and iterations_passed_in > 0: 
 		for the_iterations in range (0, iterations_passed_in):
 			the_iterations_float = float(the_iterations)
@@ -26,13 +28,13 @@ def calculate_pi(iterations_passed_in):
 	else:
 		print 'Invalid iterations'
 
-
+# Function that does the math involved in the gregory leibniz addition to be looped in calculate_pi()
 def math_calculation(exponent):
 	numerator = ((-1)**exponent)
 	denominator = numerator / ((2*exponent) + 1)
 	return denominator
 
-# Variables
+# Variable that saves the iteration that the user wants
 iterations = input('What are the number of iterations that you need: ')
 
 # Calling function
